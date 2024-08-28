a40330573517464d60c1fe960e4aeafb54fd87e1
RedOrbweaver <Oliwier.Ciesielski@protonmail.com>
2024-08-28 17:01:25 +0200

    I really overdid this test project, haven't I?

--- server.py
+++ server.py
@@ -17,20 +17,32 @@ def loadlines(filename: str):
         contents = str(file.read())
     for line in contents.split("\n"):
         if(line != "" and line != " " and line != "\n" and line.replace(" ", "") != ""):
-            line = line.replace("\"", "&quot").replace("\'", "&quot").replace("&", "&amp")
             ret.append(line.replace("\\n", "\n"))
     return ret
 
 
+def randnotlast(start, end, last):
+    if(end-start <= 1):
+        return 0
+    n = 0
+    for i in range(0, 10):
+        n = random.randrange(start, end)
+        if(n != last):
+            return n
+    if(n == 0):
+        return n+1
+    return n-1
 
 class MyServer(BaseHTTPRequestHandler):
+    lastq=-1
+    lasth=-1
     def do_GET(self):
         headers = loadlines("Headers")
         quotes = loadlines("Quotes")
-        headquote = headers[random.randrange(0, len(headers))]
-        bodyquote = quotes[random.randrange(0, len(quotes))]
+        headquote = headers[lastq := randnotlast(0, len(headers), self.lastq)]
+        bodyquote = quotes[lasth := randnotlast(0, len(quotes), self.lasth)]
         self.send_response(200)
-        self.send_header("Content-type", "text/html")
+        self.send_header("Content-type", "text/html; charset=UTF-8")
         self.end_headers()
         self.wfile.write(bytes("<html><head><title>" + headquote + "</title></head>", "utf-8"))
         self.wfile.write(bytes("<body>", "utf-8"))        
@@ -39,9 +51,13 @@ class MyServer(BaseHTTPRequestHandler):
         if(bodyquote[-1] != "." and bodyquote[-1] != "!" and bodyquote[-1] != "?"):
             bodyquote += "."
         for line in bodyquote.split("\n"):
-            self.wfile.write(bytes("<p>" + line + "</p>", "utf-8"))
+            if(line.find("<p") > 0 and line.find("</p>") > 0):
+                self.wfile.write(bytes(line, "utf-8"))
+            else:
+                self.wfile.write(bytes("<p>" + line + "</p>", "utf-8"))
         self.wfile.write(bytes("</body></html>", "utf-8"))
-
+    # def __init__():
+    #     self.lastq=
 if __name__ == "__main__":        
     webServer = HTTPServer((hostName, serverPort), MyServer)
     print("Server started http://%s:%s" % (hostName, serverPort))
